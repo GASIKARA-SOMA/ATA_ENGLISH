8f60aca5adc25d4f94cc020d87a99a6ba941460f
GASIKARA-SOMA <herraMajuro@gmail.com>
2026-02-05 23:03:03 +0300

    Implement user registration and login routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,1 +1,51 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
+const router = express.Router();
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const exist = await User.findOne({ email });
+    if (exist) return res.status(400).json({ message: "Email already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword
+    });
+
+    res.status(201).json({ message: "User created ✅", user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Wrong password" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d"
+    });
+
+    res.json({ message: "Login success ✅", token, user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export default router;
