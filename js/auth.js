5cf240e0d67c8a4b90bf425f4b8a3d939f20bdf8
GASIKARA-SOMA <herraMajuro@gmail.com>
2026-02-06 11:21:59 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,19 @@
+document.getElementById('loginBtn').addEventListener('click', async () => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
+    const res = await fetch('https://ata-english-ss1l.onrender.com/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password })
+    });
+
+    const data = await res.json();
+    if (data.token) {
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('role', data.role);
+        window.location.href = 'index.html';
+    } else {
+        alert('Erreur de connexion');
+    }
+});
